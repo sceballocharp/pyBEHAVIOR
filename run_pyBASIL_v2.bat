665a89541edf1b0fdef7611ca20e1916aaa738fc
sbagur <bagur.sophie@gmail.com>
2026-05-27 12:05:08 +0200

    ok

--- run_pyBASIL_v2.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set "SCRIPT_DIR=%~dp0"
-"%SCRIPT_DIR%.venv\Scripts\python.exe" "%SCRIPT_DIR%pyBASIL_v2.py"
--- run_pyBASIL_v3.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set "SCRIPT_DIR=%~dp0"
-"%SCRIPT_DIR%.venv\Scripts\python.exe" "%SCRIPT_DIR%pyBEHAVIOR_v3.py"
